71fb340186551a111e65a7fb8c4509b90cad7978
Tushar Mishra <tushar.140498@gmail.com>
2026-06-13 13:24:25 +0530

    feat: initial AURA agent setup — AuraState + dataloader + code_writer nodes

---
+++ agent.py
@@ -0,0 +1,52 @@
+from typing import TypedDict
+import pandas as pd
+import os
+from dotenv import load_dotenv as de
+from groq import Groq
+
+de()
+client=Groq(api_key=os.getenv("GROQ_API_KEY"))
+class aurastate (TypedDict):
+    Question: str
+    data: str
+    code: str
+    analysis: str
+    insights: str
+
+def dataloader(state: aurastate) :
+    df=pd.read_csv("HR_Employee_Attrition.csv")
+    col=df.columns.tolist()
+    return {"data": f"Columns: {str(col)}, Rows: {len(df)}"}
+
+def code_writer(state: aurastate):
+    question=state["Question"]
+    data=state["data"]
+    message=[{"role": "system","content":"""You are a Python data analyst.
+User will give you a question and dataset columns.
+Return ONLY executable Python pandas code — no explanation, no markdown, no text.
+We have already imported pandas as pd and the dataframe is already loaded as variable 'df'.
+Do not include import pandas and pd.read_csv() in your code.Do not use print() statements. Store the final result in a variable called 'result'.""" }]
+    message.append({"role": "user","content": question+ data})
+    response=client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=message
+    )
+    return response.choices[0].message.content
+
+test_state = {
+    "Question": "show attrition by department",
+    "data": "",
+    "code": "",
+    "analysis": "",
+    "insights": ""
+}
+
+state_after_load = dataloader(test_state)
+
+# Step 2 — state merge karo
+test_state.update(state_after_load)
+
+# Step 3 — code_writer chalao updated state ke saath
+state_after_code = code_writer(test_state)
+
+print(state_after_code)
